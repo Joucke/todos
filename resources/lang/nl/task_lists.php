5204ec7dc8de9c8fe81b0b94994848c3ec59397b
Joucke Hempenius <joucke@gmail.com>
2019-02-18 17:43:18 +0100

    Add status messages

--- resources/lang/nl/task_lists.php
+++ resources/lang/nl/task_lists.php
@@ -3,11 +3,17 @@
 return [
     'task_lists' => 'Lijsten',
     'create' => 'Lijst toevoegen',
+    'add' => 'Toevoegen',
     'edit' => 'Lijst bewerken',
     'update' => 'Lijst bijwerken',
     'delete' => 'Lijst verwijderen',
     'title' => 'Titel',
     'placeholders' => [
         'title' => 'Titel van de lijst',
+    ],
+    'statuses' => [
+        'created' => 'De lijst is aangemaakt',
+        'updated' => 'De lijst is bijgewerkt',
+        'deleted' => 'De lijst is verwijderd',
     ],
 ];
