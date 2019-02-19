713b7228733a0354df0b837b13ca1ad11938cf74
Joucke Hempenius <joucke@gmail.com>
2019-02-19 13:15:15 +0100

    Update Group forms, add validation

--- resources/views/groups/create.blade.php
+++ resources/views/groups/create.blade.php
@@ -1,28 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-<header class="flex justify-between items-center">
-    <h1 class="page-header">{{ __('groups.create') }}</h1>
-</header>
-<div class="py-4">
-    <div class="card-container">
-        <div class="card-padding mb-4">
-            <form action="{{ route('groups.store') }}" method="POST" class="card bg-white">
-                @csrf
-                <div class="card-body flex flex-col">
-                    <label class="mb-1" for="title">{{ __('groups.title') }}:</label>
-                    <input
-                        class="border rounded py-2 px-2"
-                        placeholder="{{ __('groups.placeholders.title') }}"
-                        type="text"
-                        name="title"
-                        value="{{ old('title') }}">
-                </div>
-                <div class="card-footer">
-                    <input class="button button-blue w-full rounded-t-none" type="submit" value="{{ __('groups.create') }}">
-                </div>
-            </form>
-        </div>
+<div class="card-container">
+    <div class="card-padding mb-4 lg:w-1/2 mx-auto">
+        <form action="{{ route('groups.store') }}" method="POST" class="card bg-white">
+            @csrf
+
+            <div class="card-header bg-grey-lighter border-b">
+                <p class="font-semibold">{{ __('groups.create') }}</p>
+            </div>
+
+            <div class="card-body flex flex-col">
+                <input
+                    class="border rounded py-2 px-2"
+                    placeholder="{{ __('groups.placeholders.title') }}"
+                    type="text"
+                    name="title"
+                    value="{{ old('title') }}">
+
+                @if ($errors->has('title'))
+                    <p class="text-red text-sm my-2" role="alert">
+                        <strong>{{ $errors->first('title') }}</strong>
+                    </p>
+                @endif
+            </div>
+
+            <div class="card-footer">
+                <input class="button button-blue w-full rounded-t-none" type="submit" value="{{ __('groups.add') }}">
+            </div>
+        </form>
     </div>
 </div>
 @endsection
