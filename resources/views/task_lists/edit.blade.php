a3dd89226a00dcf627cb57b3268b56fa2fb0423d
Joucke Hempenius <joucke@gmail.com>
2019-02-19 13:15:33 +0100

    Update TaskList forms, add validation

--- resources/views/task_lists/edit.blade.php
+++ resources/views/task_lists/edit.blade.php
@@ -1,29 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<header class="flex justify-between items-center">
-    <h1 class="page-header">{{ __('task_lists.edit') }}</h1>
-</header>
-<div class="py-4">
-    <div class="card-container">
-        <div class="card-padding mb-4">
-			<form action="{{ route('task_lists.update', $task_list) }}" method="POST" class="card bg-white">
-				@csrf
-                @method('PATCH')
-                <div class="card-body flex flex-col">
-                    <label class="mb-1" for="title">{{ __('task_lists.title') }}:</label>
-                    <input
-                        class="border rounded py-2 px-2"
-                        placeholder="{{ __('task_lists.placeholders.title') }}"
-                        type="text"
-                        name="title"
-                        value="{{ old('title', $task_list->title) }}">
-                </div>
-                <div class="card-footer">
-                    <input class="button button-blue w-full rounded-t-none" type="submit" value="{{ __('task_lists.update') }}">
-                </div>
-			</form>
-		</div>
+<div class="card-container">
+    <div class="card-padding mb-4 lg:w-1/2 mx-auto">
+        <form action="{{ route('task_lists.update', $task_list) }}" method="POST" class="card bg-white">
+            @csrf
+            @method('PATCH')
+
+            <div class="card-header bg-grey-lighter border-b">
+                <p class="font-semibold">{{ __('task_lists.edit') }}</p>
+            </div>
+
+            <div class="card-body flex flex-col">
+                <input
+                    class="border rounded py-2 px-2"
+                    placeholder="{{ __('task_lists.placeholders.title') }}"
+                    type="text"
+                    name="title"
+                    value="{{ old('title', $task_list->title) }}">
+
+                @if ($errors->has('title'))
+                    <p class="text-red text-sm my-2" role="alert">
+                        <strong>{{ $errors->first('title') }}</strong>
+                    </p>
+                @endif
+            </div>
+
+            <div class="card-footer">
+                <input class="button button-blue w-full rounded-t-none" type="submit" value="{{ __('task_lists.update') }}">
+            </div>
+		</form>
 	</div>
 </div>
 @endsection
