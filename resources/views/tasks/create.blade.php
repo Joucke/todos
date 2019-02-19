3fe70f090fa9a8387994316a5f2baa33eaa86664
Joucke Hempenius <joucke@gmail.com>
2019-02-19 13:15:46 +0100

    Update Task forms, add validation

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,18 +2,24 @@
 
 @section('content')
 <header class="flex justify-between items-center">
-    <h1 class="page-header">{{ __('tasks.create') }}</h1>
+    <h1 class="page-header">
+        <a class="text-black no-underline" href="{{ route('groups.show', $task_list->group->id) }}">{{ $task_list->group->title }}</a>
+        <span>-</span>
+        <a class="text-black no-underline" href="{{ route('task_lists.show', $task_list->id) }}">{{ $task_list->title }}</a>
+        <span>-</span>
+        <span class="">{{ __('tasks.create') }}</span>
+    </h1>
 </header>
-<div class="py-4">
-    <div class="card-container">
-        <div class="card-padding-full mb-4">
-        	<task-form
-                class="card bg-white"
-                :task-data="{title:'',interval:1,data:{}}"
-                action="{{ route('task_lists.tasks.store', $task_list) }}"
-                >
-            </task-form>
-        </div>
+
+<div class="card-container py-4">
+    <div class="card-padding-full mb-4">
+    	<task-form
+            class="card bg-white"
+            form-title="{{ __('tasks.create') }}"
+            :task-data="{title:'',interval:1,data:{}}"
+            action="{{ route('task_lists.tasks.store', $task_list) }}"
+            >
+        </task-form>
     </div>
 </div>
 @endsection
