95b7f8132acb6365beefcba7562e118527a614a7
Joucke Hempenius <joucke@gmail.com>
2019-02-20 16:07:22 +0100

    Cleanup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,42 +7,4 @@
     :cards="{{ $tasks }}"
     >
 </tabbed-cards>
-{{--
-    @foreach ($my_groups['scheduled'] as $group)
-        <div>
-            <h1>{{ $group->title }}</h1>
-            <div class="card-container">
-                @foreach ($group->tasks as $task)
-                    @if ($task->incompleted_scheduled_tasks->count())
-                        <task-card
-                            :show-title="true"
-                            :task-data="{{ $task }}"
-                            :clickable="true"
-                            >
-                        </task-card>
-                    @endif
-                @endforeach
-            </div>
-        </div>
-    @endforeach
-
-
-    @foreach ($my_groups['unscheduled'] as $group)
-        <div>
-            <h1>{{ $group->title }}</h1>
-            <div class="card-container">
-                @foreach ($group->tasks as $task)
-                    @if (!$task->incompleted_scheduled_tasks->count())
-                        <task-card
-                            :show-title="true"
-                            :task-data="{{ $task }}"
-                            :clickable="true"
-                            >
-                        </task-card>
-                    @endif
-                @endforeach
-            </div>
-        </div>
-    @endforeach
- --}}
 @endsection
